a6b0e0559d66759f8d5945762221cee9f341e2ce
Mugilan <mugilanmsm2000@gmail.com>
2023-09-10 17:41:25 +0530

    API URL changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react'
 import apiRequest from './apiRequest';
 
 function App() {
-  const API_URL = "http://localhost:3500/items";
+  const API_URL = "https://my-json-server.typicode.com/mugilan2000/todolist/items";
   const year = new Date();
   const [items, setItems] = useState([]);
 
