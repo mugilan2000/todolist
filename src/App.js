3a5e605fa711cdfd129b587eb23dcbbaeef98b00
Mugilan <mugilanmsm2000@gmail.com>
2023-09-11 21:10:33 +0530

    updated to localstorage

--- src/App.js
+++ src/App.js
@@ -2,43 +2,43 @@ import Additem from './Additem';
 import './App.css';
 import Content from './Content';
 import Footer from './Footer';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
   const year = new Date();
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo_list')) || []);
 
   const [newItem, setNewItem] = useState('')
 
-  useEffect(() => {
-    JSON.parse(localStorage.getItem('todo_list'))
-  }, [])
+  // useEffect(() => {
+  //   JSON.parse(localStorage.getItem('todo_list'))
+  // }, [])
 
-  const addItems = async (item) => {
+  const addItems = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const addNewItem = { id, checked: false, item }
     const listItems = [...items, addNewItem]
     setItems(listItems)
 
-    localStorage.setItem("todo_list",JSON.stringify(listItems))
+    localStorage.setItem("todo_list", JSON.stringify(listItems))
   }
 
-  const handleCheck = async (id) => {
+  const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     )
     setItems(listItems);
 
-    localStorage.setItem("todo_list",JSON.stringify(listItems))
+    localStorage.setItem("todo_list", JSON.stringify(listItems))
 
   }
-  const handleDelete = async (id) => {
+  const handleDelete = (id) => {
     const deleteItems = items.filter((item) =>
       item.id !== id
     )
     setItems(deleteItems);
 
-    localStorage.setItem("todo_list",JSON.stringify(deleteItems))
+    localStorage.setItem("todo_list", JSON.stringify(deleteItems))
   }
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -59,14 +59,12 @@ function App() {
         year={year}
         length={items.length}
       />
-      <main>
-        
-        <Content
-          items={items}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
-      </main>
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+
       <Footer
         length={items.length}
       />
